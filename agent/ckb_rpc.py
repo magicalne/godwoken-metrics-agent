9040db62c526af4ed5e855efd2f3fa432c3acbb5
magicalne <magicalne@gmail.com>
2022-09-28 14:50:47 +0800

    chore: remove useless code

--- agent/ckb_rpc.py
+++ agent/ckb_rpc.py
@@ -7,19 +7,6 @@ class CkbRpc(JsonRPC):
 
     def get_transaction(self, tx):
         return self.submit(method="get_transaction", params=[tx])
-        ##headers = {"Content-Type": "application/json"}
-        ##payload = {
-        ##    "id": 1,
-        ##    "jsonrpc": "2.0",
-        ##    "method": "get_transaction",
-        ##    "params": [tx],
-        ##}
-        ##try:
-        ##    r = requests.post(url="%s" % (self.url), json=payload, headers=headers)
-        ##    return r.json()
-        ##except:
-        ##    logging.error("Error getting transaction: %s", tx, exc_info=True)
-        ##    return {"result": "-1"}
 
     def get_live_cell(self, index, tx_hash):
         req = {
@@ -27,18 +14,3 @@ class CkbRpc(JsonRPC):
                 "index": index
         }
         return self.submit(method="get_live_cell", params=[req, True])
-        ##headers = {"Content-Type": "application/json"}
-        ##payload = {
-        ##    "id": 1,
-        ##    "jsonrpc": "2.0",
-        ##    "method": "get_live_cell",
-        ##    "params": [{"tx_hash": tx_hash, "index": index}, True],
-        ##}
-        ##try:
-        ##    r = requests.post(url="%s" % (self.url), json=payload, headers=headers)
-
-        ##    return r.json()
-        ##except Exception:
-        ##    logging.error("Error getting live cell.", exc_info=True)
-
-        ##    return {"result": "-1"}
--- agent/godwoken_rpc.py
+++ agent/godwoken_rpc.py
@@ -1,6 +1,3 @@
-from sys import meta_path
-import requests
-import logging
 from agent.utils import convert_int
 from agent.json_rpc import JsonRPC
 
@@ -21,89 +18,15 @@ class GodwokenRpc(JsonRPC):
 
     def gw_get_block_committed_info(self, block_hash):
         return self.submit(method="gw_get_block_committed_info", params=[block_hash])
-        #headers = {"Content-Type": "application/json"}
-        #payload = {
-        #    "id": 1,
-        #    "jsonrpc": "2.0",
-        #    "method": "gw_get_block_committed_info",
-        #    "params": [block_hash],
-        #}
-        #try:
-        #    r = requests.post(url="%s" % (self.url),
-        #                      json=payload,
-        #                      headers=headers)
-        #    return r.json()
-        #except:
-        #    logging.exception("Error get block committed info. block hash %s",
-        #                      block_hash)
-        #    return {"result": "-1"}
 
     def get_block_by_number(self, number):
         return self.submit(method="gw_get_block_by_number", params=[number])
-        
-        ##headers = {"Content-Type": "application/json"}
-        ##data = (
-        ##    '{"id":2, "jsonrpc":"2.0", "method":"gw_get_block_by_number", "params":["%s"]}'
-        ##    % (number))
-        ##try:
-        ##    r = requests.post(url="%s" % (self.web3_url),
-        ##                      data=data,
-        ##                      headers=headers)
-        ##    replay = r.json()["result"]
-        ##    print(replay)
-        ##    return {
-        ##        "blocknumber":
-        ##        convert_int(replay["block"]["raw"]["number"]),
-        ##        "commit_transactions":
-        ##        len(replay["block"]["transactions"]),
-        ##        "transactions":
-        ##        replay["block"]["transactions"],
-        ##        "blocknumber_timestamp":
-        ##        convert_int(replay["block"]["raw"]["timestamp"]),
-        ##    }
-        ##except:
-        ##    return {
-        ##        "blocknumber": "-1",
-        ##        "commit_transactions": "-1",
-        ##        "transactions": [],
-        ##        "blocknumber_timestamp": "-1",
-        ##    }
 
     def get_tip_block_hash(self):
         return self.submit(method="gw_get_tip_block_hash", params=[])
-        ##headers = {"Content-Type": "application/json"}
-        ##payload = {
-        ##    "id": 1,
-        ##    "jsonrpc": "2.0",
-        ##    "method": "gw_get_tip_block_hash",
-        ##    "params": [],
-        ##}
-        ##try:
-        ##    r = requests.post(url="%s" % (self.url),
-        ##                      json=payload,
-        ##                      headers=headers)
-        ##    return r.json()
-        ##except:
-        ##    logging.exception("Error get tip block hash")
-        ##    return {"result": "-1"}
 
     def get_block(self, block_hash):
         return self.submit(method="gw_get_block", params=[block_hash])
-        #headers = {"Content-Type": "application/json"}
-        #payload = {
-        #    "id": 1,
-        #    "jsonrpc": "2.0",
-        #    "method": "gw_get_block",
-        #    "params": [block_hash]
-        #}
-        #try:
-        #    r = requests.post(url="%s" % (self.url),
-        #                      json=payload,
-        #                      headers=headers)
-        #    return r.json()
-        #except:
-        #    logging.exception("Error get block")
-        #    return {"result": "-1"}
 
     def get_tip_number(self):
         tip_block_hash = self.get_tip_block_hash()
